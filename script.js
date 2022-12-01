b8e8adbd2a3f10080b3fb2286725734c927a62cf
mananbahl <mananbahl26@gmail.com>
2022-12-02 01:25:46 +0530

    Start

---
+++ script.js
@@ -0,0 +1,76 @@
+console.log("Welcome to Spotify");
+let audioElement = new Audio('songs/1.mp3');
+//Songs
+let masterPlay=document.getElementById('masterPlay');
+let masterForward=document.getElementById('forward');
+let masterBackward=document.getElementById('backward');
+let myProgressBar=document.getElementById('myProgressBar');
+let gif=document.getElementById('gif');
+let songItems=Array.from(document.getElementsByClassName('songItem'));
+
+let songs=[
+    {songName: "Om Deva Deva", filePath:"songs/1.mp3", coverPath:"covers/1.jpg"},
+    {songName: "BATMAN", filepath:"songs/2.mp3", coverPath:"covers/2.jpg"},
+    {songName: "A Man Without Love", filePath:"songs/3.mp3", coverPath:"covers/3.jpg"},
+    {songName: "Laut Aao Mahi Bhai", filePath:"songs/4.mp3", coverPath:"covers/4.jpg"},
+    {songName: "Laagi Chhoote", filePath:"songs/5.mp3", coverPath:"covers/5.jpg"},
+    {songName: "Spotify Wrap Banane Wale Ladke", filePath:"songs/6.mp3", coverPath:"covers/6.jpg"},
+    {songName: "Gypsy", filePath:"songs/7.mp3", coverPath:"covers/7.jpg"},
+    {songName: "Kamariya Teri ", filePath:"songs/8.mp3", coverPath:"covers/8.jpg"},
+    {songName: "Alag Aasmaan", filePath:"songs/9.mp3", coverPath:"covers/9.jpg"},
+    {songName: "The Boys Aai Aai Aa", filePath:"songs/10.mp3", coverPath:"covers/10.jpg"}
+];
+
+songItems.forEach((element,i)=>{
+    element.getElementsByTagName("img")[0]=songs[i].coverPath;
+    element.getElementsByClassName("songName")[0].innerText=songs[i].songName;
+    
+});
+//audioElement.play();
+gif.style.opacity=0;
+masterPlay.addEventListener('click',()=>{
+if(audioElement.paused || audioElement.currentTime<=0)
+{
+    audioElement.play();
+    masterPlay.src='logo.png';
+    gif.style.opacity=100;
+}
+else{
+    audioElement.pause();
+    masterPlay.src='play.png';
+    gif.style.opacity=0;
+}
+});
+masterForward.addEventListener('click',()=>{
+});
+masterBackward.addEventListener('click',()=>{
+
+});
+
+audioElement.addEventListener('timeupdate',()=>{
+    progress=parseInt((audioElement.currentTime/audioElement.duration)*100);
+    myProgressBar.value=progress;
+})
+myProgressBar.addEventListener('change',()=>{
+    audioElement.currentTime=(myProgressBar.value*audioElement.duration)/100;
+});
+const makeAllPlays=()=>{
+
+    Array.from(document.getElementsByClassName('perSongPlay')).forEach((element)=>{
+        element.firstChild.src='play.png';
+    });        
+};
+Array.from(document.getElementsByClassName('perSongPlay')).forEach((element)=>{
+    element.addEventListener('click',(e)=>{
+        makeAllPlays();
+        console.log(e.target);
+        e.target.src='logo.png';
+    });
+});
+
+
+
+
+
+
+
